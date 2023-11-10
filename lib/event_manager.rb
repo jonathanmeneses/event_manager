ba017b57064157f902d34315261e6c16a944c7f0
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2023-11-10 13:01:12 -0600

    Phone Number Format

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,54 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+
+end
+
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+def clean_phone_number(phonenumber)
+  # <10 digits = bad
+  # 11 digits and first dig is 1 --> good
+  # 11 digits and first dig != 1 --> bad
+  # >11 digits = Bad
+  stripped_number = phonenumber.gsub(/\D/, '')
+  if stripped_number.length == 10
+    return stripped_number
+  elsif stripped_number.length == 11 && stripped_number[0] =='1'
+    return stripped_number[1..]
+  else
+    return nil
+  end
+
 end
 
 puts 'EventManager initialized.'
@@ -16,23 +59,24 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  begin
-    legislators = civic_info.representative_info_by_address(
-      address: zipcode,
-      levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislator = legislators.officials
-    legislator_names = legislators.map(&:name)
+  phonenumber = clean_phone_number(row[:homephone])
+  p phonenumber
 
-  rescue
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
-  end
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislator_names}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
+
+
 end
